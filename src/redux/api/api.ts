b4c5f6ec0452d13654b486a7ce2653d1d650f6f3
misbah-ul-hoq <extraordinarymisbah@gmail.com>
2025-03-12 23:04:02 +0600

    updated production url

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseURL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:5000/api"
-    : "https://gsynctds-api.vercel.app/api";
+    : "https://api-tuitionbatch.vercel.app/api";
 
 export const baseAPI = createApi({
   reducerPath: "baseAPI",
